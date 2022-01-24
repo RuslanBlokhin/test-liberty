b16f0dd1378c94853a5264145effdddae1a5c47a
Russel <blokhin.rus@gmail.com>
2022-01-24 15:40:37 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass = require("gulp-sass")(require("sass"));
 const csso = require("gulp-csso");
 const include = require("gulp-file-include");
 const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify-es").default;
+const babel = require("gulp-babel");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
@@ -36,6 +38,18 @@ function scss() {
     .pipe(dest("dist"));
 }
 
+function js() {
+  return src("src/**.js")
+    .pipe(uglify())
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("index.js"))
+    .pipe(dest("dist"));
+}
+
 function clear() {
   return del("dist");
 }
@@ -47,8 +61,9 @@ function serve() {
 
   watch("src/**.html", series(html)).on("change", sync.reload);
   watch("src/sass/**.scss", series(scss)).on("change", sync.reload);
+  watch("src/**.js", series(js)).on("change", sync.reload);
 }
 
-exports.build = series(clear, scss, html);
-exports.serve = series(clear, scss, html, serve);
+exports.build = series(clear, scss, html, js);
+exports.serve = series(clear, scss, html, js, serve);
 exports.clear = clear;
