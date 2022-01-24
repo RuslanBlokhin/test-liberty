f71f084f3e683dd99181e50d7173b0a91cdb62c4
Russel <blokhin.rus@gmail.com>
2022-01-24 21:17:19 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const uglify = require("gulp-uglify-es").default;
 const babel = require("gulp-babel");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
+const webp = require("gulp-webp");
+const newer = require("gulp-newer");
 const del = require("del");
 const sync = require("browser-sync").create();
 
@@ -27,10 +29,23 @@ function html() {
 
 function scss() {
   return src("src/sass/**.scss")
-    .pipe(sass())
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
     .pipe(
       autoprefixer({
-        cascade: false,
+        overrideBrowserslist: ["last 8 versions"],
+        browsers: [
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 11",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6",
+        ],
       })
     )
     .pipe(csso())
@@ -50,6 +65,27 @@ function js() {
     .pipe(dest("dist"));
 }
 
+function img() {
+  return (
+    src("src/images/**.png")
+      .pipe(newer("dist/images"))
+      .pipe(webp())
+      .pipe(src("src/images/pictures/**.png"))
+      .pipe(newer("dist/images"))
+      // .pipe(
+      //   imagemin({
+      //     progressive: true,
+      //     svgoPlugins: [{ removeViewBox: false }],
+      //     interlaced: true,
+      //     optimizationLevel: 3,
+      //   })
+      // )
+      // .pipe(dest("dist/images"))
+      .pipe(src("src/images/icons/**.svg"))
+      .pipe(dest("dist/images"))
+  );
+}
+
 function clear() {
   return del("dist");
 }
@@ -59,11 +95,13 @@ function serve() {
     server: "./dist",
   });
 
-  watch("src/**.html", series(html)).on("change", sync.reload);
-  watch("src/sass/**.scss", series(scss)).on("change", sync.reload);
-  watch("src/**.js", series(js)).on("change", sync.reload);
+  watch("src/**/*.html", series(html)).on("change", sync.reload);
+  watch("src/sass/**/*.scss", series(scss)).on("change", sync.reload);
+  watch("src/**/*.js", series(js)).on("change", sync.reload);
+  watch("src/images/pictures/**/*.png", series(img)).on("change", sync.reload);
+  watch("src/images/icons/**/*.svg", series(img)).on("change", sync.reload);
 }
 
-exports.build = series(clear, scss, html, js);
-exports.serve = series(clear, scss, html, js, serve);
+exports.build = series(clear, scss, html, js, img);
+exports.serve = series(clear, scss, html, js, img, serve);
 exports.clear = clear;
