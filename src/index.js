0693d67e08aee187547a9e9f695af22462dfac98
Russel <blokhin.rus@gmail.com>
2022-01-27 15:37:43 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-const title = document.querySelector(".title");
+function getVals() {
+  const sliderStart = document.querySelector(".start");
+  const sliderEnd = document.querySelector(".end");
+  const parent = this.parentNode;
+  const slides = parent.getElementsByTagName("input");
+  const slide1 = parseFloat(slides[0].value);
+  const slide2 = parseFloat(slides[1].value);
+
+  if (slide1 > slide2) {
+    const tmp = slide2;
+    slide2 = slide1;
+    slide1 = tmp;
+  }
+
+  sliderStart.value = slide1;
+  sliderEnd.value = slide2;
+}
+
+window.onload = function () {
+  const sliderSections = document.getElementsByClassName("range-slider");
+  for (let x = 0; x < sliderSections.length; x++) {
+    const sliders = sliderSections[x].getElementsByTagName("input");
+    for (let y = 0; y < sliders.length; y++) {
+      if (sliders[y].type === "range") {
+        sliders[y].oninput = getVals;
+        sliders[y].oninput();
+      }
+    }
+  }
+};
